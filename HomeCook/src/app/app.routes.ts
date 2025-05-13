6b5b0253f2385312d06f60fdff0e9053945ed54c
Greta <g.piccoli002@studenti.unibs.it>
2025-05-13 22:39:42 +0100

    Homepage

--- HomeCook/src/app/app.routes.ts
+++ HomeCook/src/app/app.routes.ts
@@ -1,10 +1,13 @@
 import { Routes } from '@angular/router';
 import { RegisterPage } from './pages/register/register.page';
 import { RecipePage } from './pages/recipe/recipe.page';
+import {HomePage} from "./pages/home/home.page";
 
 
 export const routes: Routes = [
   { path: '', redirectTo: 'register', pathMatch: 'full' },
   { path: 'register', component: RegisterPage },
-  { path: 'recipe', component: RecipePage }
+  { path: 'recipe', component: RecipePage },
+  { path: 'home', component: HomePage }
+
 ];
